50f3f9781f2b38f1466570977977c6828c3a15aa
Nishant kumar <55913847+Nish76ant@users.noreply.github.com>
2020-08-25 11:03:31 +0530

    Create Request Module in Python.py

---
+++ Request Module in Python.py
@@ -0,0 +1,14 @@
+# Requests is an elegant and simple HTTP library for Python, built for human beings.
+import requests
+r = requests.get(
+    'https://financialmodelingprep.com/api/v3/income-statement/AAPL?apikey=demo')
+print(r.text)
+print(r.status_code)  # Request was okay means get 200
+
+# Post Requests
+url = 'www.facebook.com'
+data = {
+    "p1": 7691026822,
+    "p2": "Samsung???@123"
+}
+r2 = requests.post(url=url, data=data)
